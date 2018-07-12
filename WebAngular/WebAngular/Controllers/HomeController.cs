c19c12c3ab646215a07e9bd4472a92f539266fea
jbelodelf <jbelodelf@gmail.com>
2018-07-11 21:57:26 -0300

    Add files via upload

---
+++ WebAngular/WebAngular/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAngular.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AngularTeste()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AngularTeste(teste testeModel)
+        {
+            var teste = 1 + 1;
+            return View();
+        }
+
+
+    }
+
+    public class teste
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
